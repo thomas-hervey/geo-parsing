b875655a63c52cc577812a0bd6c38dc3e4d177be
Thomas Hervey <thomasahervey@gmail.com>
2019-02-15 10:15:20 -0700

    trying to remove large file commit

--- src/models/openData_domains.js
+++ src/models/openData_domains.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize')
 const model = {
 
   sql: {
-    table_name: 'openData_sites',
+    table_name: 'openData_domains',
 
     columns: {
       id: {
@@ -11,28 +11,18 @@ const model = {
         primaryKey: true,
         allowNull: true
       },
-      name: {
+      hostname: {
         type: Sequelize.STRING,
         primaryKey: false,
         allowNull: true
       },
-      ll_lat: {
-        type: Sequelize.FLOAT,
+      siteId: {
+        type: Sequelize.STRING,
         primaryKey: false,
         allowNull: true
       },
-      ll_lon: {
-        type: Sequelize.DECIMAL,
-        primaryKey: false,
-        allowNull: true
-      },
-      ur_lat: {
-        type: Sequelize.DECIMAL,
-        primaryKey: false,
-        allowNull: true
-      },
-      ur_lon: {
-        type: Sequelize.DECIMAL,
+      siteTitle: {
+        type: Sequelize.STRING,
         primaryKey: false,
         allowNull: true
       },
@@ -41,27 +31,7 @@ const model = {
         primaryKey: false,
         allowNull: true
       },
-      orgName: {
-        type: Sequelize.STRING,
-        primaryKey: false,
-        allowNull: true
-      },
-      size: {
-        type: Sequelize.INTEGER(11),
-        primaryKey: false,
-        allowNull: true
-      },
-      siteUrl: {
-        type: Sequelize.STRING,
-        primaryKey: false,
-        allowNull: true
-      },
-      region: {
-        type: Sequelize.STRING,
-        primaryKey: false,
-        allowNull: true
-      },
-      sector: {
+      orgTitle: {
         type: Sequelize.STRING,
         primaryKey: false,
         allowNull: true
@@ -71,7 +41,7 @@ const model = {
     // create sequel model
     createModel: async (sequelize, columns) => {
       try {
-        const Model = await sequelize.define('openData_sites', columns,
+        const Model = await sequelize.define('openData_domains', columns,
         {
           timestamps: false,
         })
